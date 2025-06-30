2097619079fdc807c06beecbadfa657f867f26bb
duckvhuynh <duchuynhvan124@gmail.com>
2025-06-30 14:17:02 +0700

    feat: map info

--- src/components/map/MapInfo.tsx
+++ src/components/map/MapInfo.tsx
@@ -6,23 +6,30 @@ interface MapFooterProps {
 
 export function MapFooter({ className }: MapFooterProps) {
     return (
-        <MapControl position={google.maps.ControlPosition.BOTTOM_CENTER}>
+        <MapControl position={google.maps.ControlPosition.BOTTOM_RIGHT}>
             <div className={`bg-transparent bg-opacity-90 rounded-t-lg pb-1 ${className}`}>
                 <div className="flex items-center space-x-2">
-                    <div className="w-16 h-8 flex items-center justify-center">
-                        <img
-                            src="/logo.png"
-                            alt="1022 Logo"
-                            className="w-full h-full object-contain"
-                        />
-                    </div>
-                    <div className="max-w-[200px]">
-                        <p className="text-xs text-muted-foreground leading-tight" style={{
-                            textShadow: '0 0 2px white, 0 0 2px white, 0 0 2px white',
-                        }}>
-                            Trung tâm Thông tin và giám sát, điều hành thông minh Đà Nẵng
-                        </p>
-                    </div>
+                    <a 
+                        href="https://1022.vn" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        className="cursor-pointer flex items-center space-x-2"
+                    >
+                        <div className="w-16 h-8 flex items-center justify-center">
+                            <img
+                                src="/logo.png"
+                                alt="1022 Logo"
+                                className="w-full h-full object-contain"
+                            />
+                        </div>
+                        <div className="max-w-[200px]">
+                            <p className="text-xs text-muted-foreground leading-tight" style={{
+                                textShadow: '0 0 2px white, 0 0 2px white, 0 0 2px white',
+                            }}>
+                                Trung tâm Thông tin và giám sát, điều hành thông minh Đà Nẵng
+                            </p>
+                        </div>
+                    </a>
                 </div>
             </div>
         </MapControl>
